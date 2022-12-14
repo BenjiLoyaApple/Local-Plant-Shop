4fd99a8e4ca9c9a5c3f3d0f26e21bb2bc29d2ab2
Benji Loya <113779387+BenjiLoyaApple@users.noreply.github.com>
2022-12-14 09:29:11 +0500

    Initial Commit

---
+++ Plant Shop Local/Plant_Shop_LocalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_Shop_LocalApp.swift
+//  Plant Shop Local
+//
+//  Created by Benji Loya on 14/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Plant_Shop_LocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
